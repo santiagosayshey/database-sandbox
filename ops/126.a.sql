6df20770d958fac37ee8e2f58064fb8bb8c9230e
santiagosayshey <schau22@pm.me>
2026-04-26 08:39:21 +0930

    a

---
+++ ops/126.a.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: a
+-- @exportedAt: 2026-04-25T23:09:18.497Z
+-- @opIds: 383
+
+-- --- BEGIN op 383 ( update quality_profile "a" )
+update "quality_profiles" set "name" = 'a' where "name" = '1080p Compact';
+-- --- END op 383
