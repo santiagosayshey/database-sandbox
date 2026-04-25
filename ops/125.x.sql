db284bc9be3ee2191f6c08517bc8917c8ebf351c
santiagosayshey <schau22@pm.me>
2026-04-26 08:36:12 +0930

    x

---
+++ ops/125.x.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: x
+-- @exportedAt: 2026-04-25T23:06:09.741Z
+-- @opIds: 378
+
+-- --- BEGIN op 378 ( update quality_profile "x" )
+update "quality_profiles" set "name" = 'x' where "name" = '1080p Balanced';
+-- --- END op 378
