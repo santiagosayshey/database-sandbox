0867c6dc65728e5e9c82bc851203c8a2e9ae75e1
santiagosayshey <schau22@pm.me>
2026-04-26 09:13:49 +0930

    tset

---
+++ ops/127.tset.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: tset
+-- @exportedAt: 2026-04-25T23:43:48.052Z
+-- @opIds: 386
+
+-- --- BEGIN op 386 ( update quality_profile "test" )
+update "quality_profiles" set "name" = 'test' where "name" = '1080p Efficient';
+-- --- END op 386
