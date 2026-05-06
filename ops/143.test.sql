a44ee6556b0e3137c90a6a54074c0e8b74f994dc
santiagosayshey <schau22@pm.me>
2026-05-06 14:31:50 +0930

    test

---
+++ ops/143.test.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: test
+-- @exportedAt: 2026-05-06T05:01:49.088Z
+-- @opIds: 986
+
+-- --- BEGIN op 986 ( update custom_format "1080p Balanced Tier 1" )
+update "custom_formats" set "description" = 'b' where "name" = '1080p Balanced Tier 1' and "description" = 'Matches release groups who fall under 1080p Balanced Tier 1';
+-- --- END op 986
